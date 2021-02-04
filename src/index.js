5707c1ca0db9f350e56af6dd085fc81f4533d86c
KeonWang <keon.wang@deltaww.com>
2021-02-04 13:47:16 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Layout from "./Layout";
+import App from './App'; //引入App
+import Progress from './Progress'
+
+
+const printMessage=()=>{
+  document.getElementById('show-area').innerHTML="我被按到了";
+}
+
+
+ReactDOM.render(
+    <div>
+      <App />
+    </div>,
+    document.getElementById('root')
+);
