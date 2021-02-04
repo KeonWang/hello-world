caa475a231df19be997d2107a32768e677c8d52f
KeonWang <keon.wang@deltaww.com>
2021-02-04 16:19:02 +0800

    rrr

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Progress from './Progress'
 
 
 const printMessage=()=>{
-  document.getElementById('show-area').innerHTML="我被按到了111222";
+  document.getElementById('show-area').innerHTML="我被按到了11122233";
 }
 
 
